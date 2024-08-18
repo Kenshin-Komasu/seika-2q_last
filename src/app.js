c74f56ce7b0dc831a9a59afeb92b440814a4a3d2
kenshin.komasu <k221t065@st.kyoto-seika.ac.jp>
2024-08-18 21:29:07 +0900

    コミットしました

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ var c = document.getElementById("c");
 var ctx = c.getContext("2d");
 var cH;
 var cW;
-var bgColor = "#FF6138";
+var bgColor = "#FF00FF";
 var animations = [];
 var circles = [];
 
 var colorPicker = (function() {
-  var colors = ["#FF6138", "#FF00FF", "#2980B9", "#282741"];
+  var colors = ["#FF00FF", "#FF00FF", "#2980B9", "#282741"];
   var index = 0;
   function next() {
     index = index++ < colors.length-1 ? index : 0;
